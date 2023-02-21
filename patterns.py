404e1ff40e36a84482c287293851bfe6e02e3d70
Nitish <nk.nitish.nk500@gmail.com>
2023-02-21 13:30:20 +0530

    Create patterns.py

---
+++ patterns.py
@@ -0,0 +1,88 @@
+# Pattern 1
+# *
+# **
+# ***
+# ****
+print("Pattern #1")
+i = 1
+while i<=4:
+    print("*"*i)
+    i += 1
+
+# Pattern 2
+# ****
+# ***
+# **
+# *
+print("Pattern #2")
+i = 4
+while i>=1:
+    print("*"*i)
+    i -= 1
+
+# Pattern 3
+#    *
+#   **
+#  ***
+# ****
+print("Pattern #3")
+i = 1
+while i<=4:
+    print(" "*(4-i), "*"*i)
+    i += 1
+
+# Pattern 4
+# ****
+#  ***
+#   **
+#    *
+print("Pattern #4")
+i = 4
+while i>=1:
+    print(" "*(4-i), "*"*i)
+    i -= 1
+
+# Pattern 5
+#    *
+#   * *
+#  * * *
+# * * * *
+print("Pattern #5")
+i = 1
+while i<=4:
+    print(" "*(4-i), "* "*i)
+    i += 1
+
+# Pattern 6
+# * * * *
+#  * * *
+#   * *
+#    *
+print("Pattern #6")
+i = 4
+while i>=1:
+    print(" "*(4-i), "* "*i)
+    i -= 1
+
+
+# Pattern 7
+#    *
+#   * *
+#  * * *
+# * * * *
+#  * * *
+#   * *
+#    *
+
+print("Pattern #7")
+
+i = 1
+while i<=3:
+    print(" "*(4-i), "* "*i)
+    i += 1
+
+i = 4
+while i>=1:
+    print(" "*(4-i), "* "*i)
+    i -= 1
+
