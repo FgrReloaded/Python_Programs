fbaa25f258f3f3c51d43927ffe31f1fb8126d1da
Nitish <nk.nitish.nk500@gmail.com>
2023-02-28 21:57:28 +0530

    Update index_word.py

--- index_word.py
+++ index_word.py
@@ -1,9 +1,16 @@
 # Find a word from a string and print it's index
 
 word = input("Enter a word: ")
-srch = input("Enter a word or letter to search")
+srch = input("Enter a word or letter to search: ")
+
+# Method 1 (For Letters and word both)
 
 for i in range(len(word)):
     if word.startswith(srch, i):
         print(i)
-        
+
+# Method 2 (For Letters only)
+
+for i in range(len(word)):
+    if word[i] == srch:
+        print(i)
