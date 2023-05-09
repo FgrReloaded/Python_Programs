9a43f3a626a5130641403eba57fc27662427f8a2
Nitish <nk.nitish.nk500@gmail.com>
2023-05-09 16:13:01 +0530

    09-05-2023

---
+++ numpy.py
@@ -0,0 +1,4 @@
+'''
+Numpy means numerical python.
+All the numerical and scientific calculation in python is done with numpy
+'''
---
+++ try_except.py
@@ -0,0 +1,8 @@
+try:
+    a = 100
+    b = a/2
+    print(b)
+except Exception as err:
+    print(err)
+finally:
+    print("Abc")
